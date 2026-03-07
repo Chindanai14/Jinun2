c57dbf62183b6c79ac103c189366f2157e12145f
Chindanai <chindanai14@gmail.com>
2026-03-07 14:29:14 +0700

    Test

---
+++ src/main/java/com/Aaeli/jinun/Jinun.java
@@ -0,0 +1,10 @@
+package com.Aaeli.jinun;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Jinun implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/Aaeli/jinun/client/JinunClient.java
@@ -0,0 +1,10 @@
+package com.Aaeli.jinun.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JinunClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
