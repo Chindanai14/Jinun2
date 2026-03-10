2963b17948d9e21bf4279ff2b05c482d64e9b153
Chindanai <chindanai14@gmail.com>
2026-03-10 18:14:45 +0700

    Initial commit

--- src/main/java/com/Aaeli/jinun/client/JinunClient.java
+++ src/main/java/com/Aaeli/jinun/client/JinunClient.java
@@ -4,7 +4,27 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class JinunClient implements ClientModInitializer {
 
+    public static final String NAME = "Jinun";
+    public static final String VERSION = "0.1";
+
+    private static JinunClient INSTANCE;
+
+    public static JinunClient getInstance() {
+        return INSTANCE;
+    }
+
     @Override
     public void onInitializeClient() {
+        INSTANCE = this;
+
+        System.out.println(NAME + " client loaded!");
+
+        initManagers();
+    }
+
+    private void initManagers() {
+        // ModuleManager.init();
+        // EventBus.init();
+        // CommandManager.init();
     }
 }
